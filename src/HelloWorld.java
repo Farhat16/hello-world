ba3bf52dd5925f4c18105902af4c669d619828a2
Farhat16 <Farhat.shameer@gmail.com>
2016-04-15 00:24:43 -0700

    Update HelloWorld.java
    RollNO 16
    Farhat  shameer

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,85 @@
-public class HelloWorld {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+
+package sortarray;
+
+public class Sortarray {
+
+public int[] array;
+
+public Sortarray(int[] m){
+
+this.arr=m;
+
+}
+
+public void display(){
+
+System.out.println("\n array ");
+
+for(int k=0;k<this.arr.length;k++)
+
+{
+System.out.print(this.arr1[k] + "  ");
+
+
+}
+
+}
+
+public void sortaraymethd()
+
+{
+
+for(int k=0;k<arr.length;k++)
+
+{
+
+for(int f=i+1;f<arr.length;f++)
+
+{
+int temp;
+
+
+if(arr[k]>arr[f])
+
+
+{
+
+
+    temp=arr[i];
+
+
+    arr[k]=arr[f];
+
+
+    arr[f]=temp;
+
+
+}
+
+
+}
+
+}
+
+}
+
+public static void main(String[] args) {
+
+int[] m={9,0,2,3,1,6,7,8,4,5};
+
+Sortaray sort=new Sortaray(m);
+
+System.out.println("\n array before sorting");
+
+sort.display();
+
+System.out.println("\n array after sorting");
+
+sort.sortaraymethd();
+
+sort.display();
+
+}
 
 }
